df980562c2d7800ec67c08d542b47f9d70d1a20b
maspi83 <69327668+maspi83@users.noreply.github.com>
2020-11-27 20:05:07 +0100

    Update ubuntu_bootstrap_kubetools.sh

--- linux_academy/ubuntu_bootstrap_kubetools.sh
+++ linux_academy/ubuntu_bootstrap_kubetools.sh
@@ -16,3 +16,7 @@ if [ "$?" -eq "0" ]; then sudo apt-mark hold kubelet kubeadm kubectl; else echo 
 
 echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
+
+echo "Installing kubens and kubectx"
+wget -q https://github.com/ahmetb/kubectx/releases/download/v0.9.1/kubens -O /usr/bin/kubens && chmod 755 /usr/bin/kubens
+wget -q https://github.com/ahmetb/kubectx/releases/download/v0.9.1/kubectx -O /usr/bin/kubectx && chmod 755 /usr/bin/kubectx
