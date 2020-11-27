5cf9c5d5f594ded800168f16c1d31752053746c5
maspi83 <69327668+maspi83@users.noreply.github.com>
2020-11-27 16:48:17 +0100

    Update ubuntu_bootstrap_k8smaster.sh

--- linux_academy/ubuntu_bootstrap_k8smaster.sh
+++ linux_academy/ubuntu_bootstrap_k8smaster.sh
@@ -1,3 +1,5 @@
+OS_CHECK=$(grep DISTRIB_ID /etc/*-release | cut -d'=' -f2)
+if [ "$OS_CHECK" != "Ubuntu" ]; then echo "Sorry this is only for Ubuntu distro's, exiting..." && exit 1; fi
 echo ""
 echo -n "K8s Master INIT"
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
