7be10b2f97fbc0b2ce73e252be1c35ee7cd1100e
maspi83 <69327668+maspi83@users.noreply.github.com>
2021-11-29 20:10:28 +0100

    Update dns_tester.sh

--- kodekloud/labs/dns_tester.sh
+++ kodekloud/labs/dns_tester.sh
@@ -4,7 +4,7 @@ sudo apt install python-pip  -y
 pip install python-numpy
 sudo apt install python-numpy  -y
 git clone https://github.com/kubernetes/perf-tests.git
-cd perf-tests/dns/perf
+cd perf-tests/dns
 sudo apt install python-yaml  -y
 sudo apt install pyyaml  -y
 pip3 install pyyaml 
