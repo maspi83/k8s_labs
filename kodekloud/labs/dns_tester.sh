c0e3f6f9cab2b746c686be6bf7f268593c671f8a
maspi83 <69327668+maspi83@users.noreply.github.com>
2021-11-29 20:12:36 +0100

    Update dns_tester.sh

--- kodekloud/labs/dns_tester.sh
+++ kodekloud/labs/dns_tester.sh
@@ -3,9 +3,9 @@ sudo apt install python3-pip -y
 sudo apt install python-pip  -y
 pip install python-numpy
 sudo apt install python-numpy  -y
-git clone https://github.com/kubernetes/perf-tests.git
-cd perf-tests/dns
 sudo apt install python-yaml  -y
 sudo apt install pyyaml  -y
 pip3 install pyyaml 
+git clone https://github.com/kubernetes/perf-tests.git
+cd perf-tests/dns
 echo "Excecute: ./run core-dns out"
